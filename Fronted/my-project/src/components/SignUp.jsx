65e71b34357ec6a5f789525df666f7b60c1e04d1
Sumit <sumit9596@gmail.com>
2026-04-18 01:35:24 +0530

    Fix: Correct formState destructuring, button properly disabled on validation errors

--- Fronted/my-project/src/components/SignUp.jsx
+++ Fronted/my-project/src/components/SignUp.jsx
@@ -6,7 +6,7 @@ import Navbar from "./Navbar";
 
 function SignUp() {
   const navigate = useNavigate();
-  const { register, handleSubmit, formState: { errors }, reset, watch } = useForm({
+  const { register, handleSubmit, formState: { errors, isValid }, reset, watch } = useForm({
     defaultValues: {
       fullname: "",
       email: "",
@@ -15,7 +15,6 @@ function SignUp() {
   });
 
   const password = watch("password");
-  const { isValid } = formState;
 
   const onSubmit = async (data) => {
     const userData = {
@@ -109,8 +108,8 @@ function SignUp() {
                 type="submit"
                 disabled={!isValid}
                 className={`text-white px-6 py-2 rounded font-semibold transition cursor-pointer ${isValid
-                    ? 'bg-blue-600 hover:bg-blue-700 cursor-pointer'
-                    : 'bg-gray-400 cursor-not-allowed opacity-60'
+                  ? 'bg-blue-600 hover:bg-blue-700 cursor-pointer'
+                  : 'bg-gray-400 cursor-not-allowed opacity-60'
                   }`}
               >
                 Sign Up
